d8917b9cedb7a8932f21599839427a7c3a96a1d1
WingedLeopard <therealfakeanthony@gmail.com>
2025-01-26 18:41:41 -0600

    rr stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Wrist.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Wrist.java
@@ -5,29 +5,25 @@ import androidx.annotation.NonNull;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.acmerobotics.roadrunner.Action;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class Wrist {
     private Servo wrist;
     private OpMode myOpMode;
+
     public Wrist(OpMode opmode) {
         myOpMode = opmode;
     }
+
     public void init() {
         // Define and Initialize Motors (note: need to use reference to actual OpMode).
         wrist = myOpMode.hardwareMap.get(Servo.class, "wrist");
-
     }
-    public class WristDown implements Action {
 
+    public class WristDown implements Action {
         @Override
         public boolean run(@NonNull TelemetryPacket telemetryPacket) {
-            wrist.setPosition(0);
+            wrist.setPosition(0); // Set wrist to the down position
             return false;
         }
     }
@@ -35,12 +31,11 @@ public class Wrist {
     public Action wristDown() {
         return new WristDown();
     }
+
     public class WristScore implements Action {
         @Override
-
         public boolean run(@NonNull TelemetryPacket packet) {
-            // desired position, actions do not have parameters (you will have to create a new action for each position you need to go to in auto)
-            wrist.setPosition(0.67);
+            wrist.setPosition(0.67); // Set wrist to the scoring position
             return false;
         }
     }
@@ -49,5 +44,15 @@ public class Wrist {
         return new WristScore();
     }
 
+    public class WristMid implements Action {
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            wrist.setPosition(0.5); // Set wrist to the mid position
+            return false;
+        }
+    }
 
+    public Action wristMid() {
+        return new WristMid();
+    }
 }
