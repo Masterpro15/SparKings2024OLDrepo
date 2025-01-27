008d96376d1026b1f57461cace3af15bc400372a
WingedLeopard <therealfakeanthony@gmail.com>
2025-01-26 18:40:22 -0600

    rr stuff

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTestingAnn.java
@@ -0,0 +1,50 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+
+public class MeepMeepTestingAnn {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(500);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-38, -61, Math.toRadians(180)))
+                //.waitSeconds(1)
+                .splineTo(new Vector2d(-57.5, -57.5), 205)
+                //score sample 1.
+                //.waitSeconds(1)
+                .strafeTo(new Vector2d(-55, -55))
+                .splineToSplineHeading(new Pose2d(new Vector2d(-48, -48), Math.toRadians(90)), Math.toRadians(90))
+                //grab sample 2
+                //.waitSeconds(1)
+                .splineTo(new Vector2d(-57.5, -57.5), 205)
+                //score sample 2
+                //.waitSeconds(1)
+                .strafeTo(new Vector2d(-55, -55))
+                .splineToSplineHeading(new Pose2d(new Vector2d(-58, -48), Math.toRadians(90)), Math.toRadians(90))
+                //grab sample 3
+                //.waitSeconds(1)
+                .splineTo(new Vector2d(-57.5, -57.5), 205)
+                //score sample 3
+                .strafeTo(new Vector2d(-45, -34))
+                .splineToSplineHeading(new Pose2d(new Vector2d(-24, 0), Math.toRadians(0)), Math.toRadians(0))
+                //get ready for teleop!
+
+
+
+                .build());
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Arm.java
@@ -9,51 +9,52 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 public class Arm {
-    final double ARM_TICKS_PER_DEGREE =
-            28 // number of encoder ticks per rotation of the bare motor
-                    * 250047.0 / 4913.0 // This is the exact gear ratio of the 50.9:1 Yellow Jacket gearbox
-                    * 100.0 / 20.0 // This is the external gear reduction, a 20T pinion gear that drives a 100T hub-mount gear
-                    * 1/360.0; // we want ticks per degree, not per rotation
-    final int ARM_COLLAPSED_INTO_ROBOT  = (int)(0*ARM_TICKS_PER_DEGREE);
-    final int ARM_SCORE_SPECIMEN        = (int)(60 * ARM_TICKS_PER_DEGREE);
-    final int ARM_SCORE_SPECIMEN2        = (int)(25 * ARM_TICKS_PER_DEGREE);
+    // Constants
+    private static final double ARM_TICKS_PER_DEGREE =
+            28 // Number of encoder ticks per rotation of the bare motor
+                    * 250047.0 / 4913.0 // Exact gear ratio of the 50.9:1 Yellow Jacket gearbox
+                    * 100.0 / 20.0 // External gear reduction (20T driving 100T hub gear)
+                    * 1 / 360.0; // Convert to ticks per degree
+
+    // Target positions
+    private static final int ARM_COLLAPSED_INTO_ROBOT = (int) (0 * ARM_TICKS_PER_DEGREE);
+    private static final int ARM_SCORE_SPECIMEN = (int) (60 * ARM_TICKS_PER_DEGREE);
+    private static final int ARM_SCORE_SPECIMEN2 = (int) (25 * ARM_TICKS_PER_DEGREE);
+    private static final int ARM_COLLECT_SAMPLE = (int) (20 * ARM_TICKS_PER_DEGREE);
+    private static final int ARM_HIGH_BASKET = (int) (90 * ARM_TICKS_PER_DEGREE);
+
     private DcMotorEx armMotor;
-    private OpMode myOpMode;
-    public Arm(OpMode opmode) {
-        myOpMode = opmode;
+    private final OpMode myOpMode;
+
+    public Arm(OpMode opMode) {
+        myOpMode = opMode;
     }
-    int armposition;
-    public void init(){
+
+    public void init() {
         armMotor = myOpMode.hardwareMap.get(DcMotorEx.class, "arm_motor");
         armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         armMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
+    // Action: Move arm down
     public class ArmDown implements Action {
         @Override
-
         public boolean run(@NonNull TelemetryPacket packet) {
-            // desired position, actions do not have parameters (you will have to create a new action for each position you need to go to in auto)
-            armMotor.setTargetPosition(ARM_COLLAPSED_INTO_ROBOT);
-            armMotor.setPower(0.7);// (adjust speed for whatever is necessary)
-            //(you cannot stop motion within this action, you would have to do it in another)
-            armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            moveToPosition(ARM_COLLAPSED_INTO_ROBOT);
             return false;
         }
     }
+
     public Action armDown() {
         return new ArmDown();
     }
+
+    // Action: Move arm up
     public class ArmUp implements Action {
         @Override
-
         public boolean run(@NonNull TelemetryPacket packet) {
-            // desired position, actions do not have parameters (you will have to create a new action for each position you need to go to in auto)
-            armMotor.setTargetPosition(ARM_SCORE_SPECIMEN);
-            armMotor.setPower(0.7);// (adjust speed for whatever is necessary)
-            //(you cannot stop motion within this action, you would have to do it in another)
-            armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            moveToPosition(ARM_SCORE_SPECIMEN);
             return false;
         }
     }
@@ -61,17 +62,61 @@ public class Arm {
     public Action armUp() {
         return new ArmUp();
     }
-    public class ArmStop implements Action{
+
+    // Action: Grab position
+    public class ArmGrab implements Action {
         @Override
         public boolean run(@NonNull TelemetryPacket packet) {
+            moveToPosition(ARM_COLLECT_SAMPLE);
+            return false;
+        }
+    }
 
+    public Action armGrab() {
+        return new ArmGrab();
+    }
+
+    // Action: Stop the arm
+    public class ArmStop implements Action {
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
             armMotor.setPower(0);
             return false;
         }
     }
-    public Action armStop(){
+
+    public Action armStop() {
         return new ArmStop();
     }
 
+    public class ArmBasket implements Action {
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            moveToPosition(ARM_HIGH_BASKET);
+            return false;
+        }
+    }
 
+    public Action armBasket() {
+        return new ArmBasket();
+    }
+
+    public class ArmCollapse implements Action {
+        @Override
+        public boolean run(@NonNull TelemetryPacket packet) {
+            moveToPosition(ARM_COLLAPSED_INTO_ROBOT);
+            return false;
+        }
+    }
+
+    public Action armCollapse() {
+        return new ArmCollapse();
+    }
+
+    // Helper method to set the arm position
+    private void moveToPosition(int position) {
+        armMotor.setTargetPosition(position);
+        armMotor.setPower(0.7); // Adjust speed as necessary
+        armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
 }
