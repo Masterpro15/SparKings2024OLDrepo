a1ae3887b3d0acd3021730842ccf7c0937783d13
Masterpro15 <evanckurian@gmail.com>
2025-02-09 17:53:09 -0600

    working on 4 :(((( in meep meep

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/newSpecAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/newSpecAuto.java
@@ -34,112 +34,113 @@ public class newSpecAuto extends LinearOpMode {
         startPose = new Pose2d(0, -61, Math.toRadians(90));
         drive = new MecanumDrive(hardwareMap, startPose);
         TrajectoryActionBuilder build = drive.actionBuilder(startPose)
-                .afterTime(0.2, arm.armNewSpec2())
-                .afterTime(0.1, claw.clawClose())
+                .afterTime(0.2, arm.armNewSpec())
+                 .afterTime(0.1, claw.clawClose())
                 .afterTime(0.1, wrist.wristNewScore())
                 .afterTime(0.1, lift.liftScoreSpec())
                 .waitSeconds(0.75)
-                .strafeTo(new Vector2d(0, -29))
-                .waitSeconds(0.1)
-                .afterTime(0.1 , claw.clawOpen())
+                .strafeTo(new Vector2d(0, -29.45))
                 .waitSeconds(0.05)
-                .afterTime(0 , wrist.wristDown())
-                .afterTime(0, lift.liftDown())
+                 .afterTime(0.1 , claw.clawOpen())
+                .waitSeconds(0.05)
+                  .afterTime(0 , wrist.wristDown())
+                   .afterTime(0, lift.liftDown())
+
                 .setReversed(true)
-                //.afterTime(0 , arm.armStop())
-                .splineToSplineHeading(new Pose2d(new Vector2d(26,-43), Math.toRadians(-90)), 0)
-                .afterTime(0 , arm.armDown())
-                .afterTime(0.1 , arm.armStop())
-                .splineToLinearHeading(new Pose2d(new Vector2d(40,-13), Math.toRadians(-90)), 0)
+                .afterTime(0 , arm.armStop())
+                .splineToSplineHeading(new Pose2d(new Vector2d(27,-43), Math.toRadians(90)), 0)
+                  .afterTime(0 , arm.armDown())
+                  .afterTime(0.1 , arm.armStop())
+                .splineToLinearHeading(new Pose2d(new Vector2d(43,-13), Math.toRadians(90)), 0)
                 .strafeTo(new Vector2d(47.5, -13))
 
 
-                //.splineTo(new Vector2d(45, -13), 0)
+                .splineTo(new Vector2d(45, -13), 0)
 
                 .strafeTo(new Vector2d(47.5,-53))
                 //one in observation zone
                 //.strafeTo(new Vector2d(45,-13))
 
-                .setReversed(true)
-                .splineToSplineHeading(new Pose2d(new Vector2d(54, -13), Math.toRadians(-90)), 0)
+                .setReversed(false)
+                .splineToLinearHeading(new Pose2d(new Vector2d(58, -13), Math.toRadians(-90)), 0)
 
                 //  .strafeTo(new Vector2d(56.5,-13))
                  .afterTime(0, wrist.wristGrab())
-                .strafeTo(new Vector2d(56.8,-56))
+                .strafeTo(new Vector2d(56.8,-58))
+                .waitSeconds(0.5)
+                  .afterTime(0.1 , claw.clawClose())
+                .waitSeconds(0.1)
+                 .afterTime(0.1 , arm.armNewSpec())
+                  .afterTime(0.1, wrist.wristNewScore())
+                  .afterTime(0.1, lift.liftScoreSpec())
+                .setReversed(true)
+
+                .splineToSplineHeading(new Pose2d(new Vector2d(1,-30), Math.toRadians(-270)), Math.toRadians(90))
+                .waitSeconds(0.05)
+                 .afterTime(0.1 , claw.clawOpen())
+                .waitSeconds(0.1)
+                 .afterTime(0 , wrist.wristGrab())
+                  .afterTime(0, lift.liftDown())
+                .setReversed(true)
+                 .afterTime(0 , arm.armDown())
+                .afterTime(0, wrist.wristGrab())
+                .splineToSplineHeading(new Pose2d(new Vector2d(43, -57), Math.toRadians(270)), Math.toRadians(270))
+                 .afterTime(0, wrist.wristGrab())
+                 .afterTime(0 , arm.armStop())
+
+                 .afterTime(0, wrist.wristGrab())
+
                 .waitSeconds(0.5)
                 .afterTime(0.1 , claw.clawClose())
                 .waitSeconds(0.1)
-                .afterTime(0.1 , arm.armNewSpec())
-                .afterTime(0.1, wrist.wristNewScore())
-                .afterTime(0.1, lift.liftScoreSpec())
+                  .afterTime(0.1 , arm.armNewSpec())
+                 .afterTime(0.1, wrist.wristNewScore())
+                 .afterTime(0.1, lift.liftScoreSpec())
+
+
+
                 .setReversed(true)
 
                 .splineToSplineHeading(new Pose2d(new Vector2d(1,-30), Math.toRadians(-270)), Math.toRadians(90))
+                .waitSeconds(0.05)
+                 .afterTime(0.1 , claw.clawOpen())
                 .waitSeconds(0.1)
-                .afterTime(0.1 , claw.clawOpen())
-                .waitSeconds(0.1)
-                .afterTime(0 , wrist.wristGrab())
-                .afterTime(0, lift.liftDown())
+                 .afterTime(0 , wrist.wristGrab())
+                  .afterTime(0, lift.liftDown())
                 .setReversed(true)
-                .afterTime(0 , arm.armDown())
+                 .afterTime(0 , arm.armDown())
                 .afterTime(0, wrist.wristGrab())
-                .splineToSplineHeading(new Pose2d(new Vector2d(43, -55), Math.toRadians(270)), Math.toRadians(270))
+                .splineToSplineHeading(new Pose2d(new Vector2d(43, -56), Math.toRadians(270)), Math.toRadians(270))
                 .afterTime(0, wrist.wristGrab())
-                .afterTime(0 , arm.armStop())
+                 .afterTime(0 , arm.armStop())
 
-                .afterTime(0, wrist.wristGrab())
-
-                .waitSeconds(0.3)
+                 .afterTime(0, wrist.wristGrab())
+                .waitSeconds(0.5)
                 .afterTime(0.1 , claw.clawClose())
-                .waitSeconds(0.15678)
-                .afterTime(0.1 , arm.armNewSpec())
-                .afterTime(0.1, wrist.wristNewScore())
-                .afterTime(0.1, lift.liftScoreSpec())
+                .waitSeconds(0.1)
+                  .afterTime(0.1 , arm.armNewSpec())
+                 .afterTime(0.1, wrist.wristNewScore())
+                 .afterTime(0.1, lift.liftScoreSpec())
+
+
+
+
+
+
                 .setReversed(true)
                 .splineToSplineHeading(new Pose2d(new Vector2d(1,-30), Math.toRadians(-270)), Math.toRadians(90))
-                .waitSeconds(0.1)
-                .afterTime(0.1 , claw.clawOpen())
-                .waitSeconds(0.1)
-                .afterTime(0 , wrist.wristGrab())
-                .afterTime(0, lift.liftDown())
+                .waitSeconds(0.05)
+                 .afterTime(0.1 , claw.clawOpen())
+                 .afterTime(0 , wrist.wristDown())
+                  .afterTime(0, lift.liftDown())
+                .waitSeconds(0.05)
+
                 .setReversed(true)
-
-
-
-                .afterTime(0 , arm.armDown())
-                .afterTime(0, wrist.wristGrab())
-                .splineToSplineHeading(new Pose2d(new Vector2d(43, -555), Math.toRadians(270)), Math.toRadians(270))
-                .afterTime(0, wrist.wristGrab())
-                .afterTime(0 , arm.armStop())
-
-                .afterTime(0, wrist.wristGrab())
-
-                .waitSeconds(0.2)
-                .afterTime(0.1 , claw.clawClose())
-                .waitSeconds(0.1)
-                .afterTime(0.1 , arm.armNewSpec())
-                .afterTime(0.1, wrist.wristNewScore())
-                .afterTime(0.1, lift.liftScoreSpec())
-                .setReversed(true)
-                .splineToSplineHeading(new Pose2d(new Vector2d(1.3,-29), Math.toRadians(-270)), Math.toRadians(90))
-                .afterTime(0 , claw.clawOpen())
-                .afterTime(0 , wrist.wristDown())
-                .afterTime(0, lift.liftDown())
-                .strafeTo(new Vector2d(50, -61))
-                .afterTime(0 , arm.armDown())
+                .splineToLinearHeading(new Pose2d( new Vector2d(60, -61), Math.toRadians(-270)), 0)
 
 
 
 
-
-                /*
-                .
-
-
-                .splineToSplineHeading(new Pose2d(new Vector2d(55,-55), Math.toRadians(270)), Math.toRadians(270))
-
-
-            */
 
 
 
